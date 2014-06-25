9d230cb8aba59f7ccf9caac6a39730b5908c00ea
Edward Ekstrom <edward.ekstrom@gmail.com>
2014-06-25 10:28:42 -0600

    commit

--- src/main/ImageEditor.java
+++ src/main/ImageEditor.java
@@ -4,10 +4,10 @@ import java.io.PrintWriter;
 
 public class ImageEditor {
 	public static void main(String[] args){
-		String inputFileString = "C:\\Users\\i53425\\Documents\\ImageEditor\\src\\main\\slctemple.ppm";
-		String outputFileString = "C:\\Users\\i53425\\Documents\\ImageEditor\\src\\main\\output.ppm";
-		String action = "motionblur";
-		int blurLength = 20;
+		String inputFileString = "/home/edwardekstrom/workspace/ImageEditor/src/main/slctemple.ppm";
+		String outputFileString = "/home/edwardekstrom/workspace/ImageEditor/src/main/output.ppm";
+		String action = "invert";
+		int blurLength = -1;
 		
 		if(args.length > 4){
 			System.out.println("too many args");
@@ -25,20 +25,8 @@ public class ImageEditor {
 		try{
 			PrintWriter writer = new PrintWriter(outputFileString, "UTF-8");
 			if(action.equals("invert")){
-                                inputImage.invert();
 				writer.print(inputImage.toString());
-			}else if(action.equals("grayscale")){
-                            inputImage.grayscale();
-                            writer.print(inputImage.toString());
-                        }else if(action.equals("emboss")){
-                            inputImage.emboss();
-                            writer.print(inputImage.toString());
-                        }else if(action.equals("motionblur")){
-                            inputImage.motionblur(blurLength);
-                            writer.print(inputImage.toString());
-                        }else{
-                            
-                        }
+			}
 		}catch (Exception e){
 			e.printStackTrace();
 		}
